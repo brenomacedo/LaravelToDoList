b5ef278a1c730074bcd2e2f6b3f8d860ec7cffcb
brenosekiro <b88660501@gmail.com>
2020-09-02 05:53:34 -0300

    form

--- resources/views/user/todolist.blade.php
+++ resources/views/user/todolist.blade.php
@@ -13,11 +13,18 @@
 </head>
 <body>
     <div class="todolist-container">
-        <a href="{{ route('logout') }}">Logout <i class="fas fa-sign-out-alt"></i></a>
-        <form method="post">
+        <div class="todolist-profilebar">
+            <h3 class="todolist-profilebar-name">{{ $user['name'] }}</h3>
+            <a href="{{ route('logout') }}">Logout <i class="fas fa-sign-out-alt"></i></a>
+        </div>
+        <form method="post" class="todolist-form">
             <input class="todolist-input" type="text" name="newtask" placeholder="Nova tarefa">
             <button class="todolist-button"><i class="fas fa-plus"></i></button>
         </form>
+        <div class="todolist-task">
+            <div class="todolist-task-item">Teste</div>
+            <button class="todolist-button-delete"><i class="fas fa-trash"></i></button>
+        </div>
     </div>
 </body>
 </html>
